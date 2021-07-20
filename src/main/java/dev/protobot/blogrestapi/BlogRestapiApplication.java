5cc8d64d2d31e8bbaba3f0c1eecfa3b5e3d0b998
julioperezdev <protobot.dev@gmail.com>
2021-07-19 23:48:28 -0300

    setuping files

---
+++ src/main/java/dev/protobot/blogrestapi/BlogRestapiApplication.java
@@ -0,0 +1,13 @@
+package dev.protobot.blogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/protobot/blogrestapi/BlogRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.protobot.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
