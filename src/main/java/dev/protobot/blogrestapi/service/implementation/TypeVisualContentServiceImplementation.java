46e73c7ab0d8a1aa02005785b945940754956a68
julioperezdev <protobot.dev@gmail.com>
2021-07-28 00:25:12 -0300

    TDD in TypeVisualContentServiceImplementation

--- src/main/java/dev/protobot/blogrestapi/service/implementation/TypeVisualContentServiceImplementation.java
+++ src/main/java/dev/protobot/blogrestapi/service/implementation/TypeVisualContentServiceImplementation.java
@@ -1,7 +1,13 @@
 package dev.protobot.blogrestapi.service.implementation;
 
+import dev.protobot.blogrestapi.helper.shared.CheckIfNullOrEmptyString;
+import dev.protobot.blogrestapi.helper.shared.CheckIfNullOrZeroLong;
+import dev.protobot.blogrestapi.helper.shared.CheckIfStringHaveNumber;
+import dev.protobot.blogrestapi.helper.shared.ConvertStringToLowerCase;
 import dev.protobot.blogrestapi.model.TypeVisualContent;
+import dev.protobot.blogrestapi.repository.TypeVisualContentRepository;
 import dev.protobot.blogrestapi.service.TypeVisualContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,24 +16,57 @@ import java.util.Optional;
 @Service
 public class TypeVisualContentServiceImplementation implements TypeVisualContentService {
 
+    private final TypeVisualContentRepository typeVisualContentRepository;
+
+    private final CheckIfNullOrEmptyString checkIfNullOrEmptyString;
+
+    private final CheckIfNullOrZeroLong checkIfNullOrZeroLong;
+
+    private final CheckIfStringHaveNumber checkIfStringHaveNumber;
+
+    private final ConvertStringToLowerCase convertStringToLowerCase;
+
+    @Autowired
+    public TypeVisualContentServiceImplementation(TypeVisualContentRepository typeVisualContentRepository, CheckIfNullOrEmptyString checkIfNullOrEmptyString, CheckIfNullOrZeroLong checkIfNullOrZeroLong, CheckIfStringHaveNumber checkIfStringHaveNumber, ConvertStringToLowerCase convertStringToLowerCase) {
+        this.typeVisualContentRepository = typeVisualContentRepository;
+        this.checkIfNullOrEmptyString = checkIfNullOrEmptyString;
+        this.checkIfNullOrZeroLong = checkIfNullOrZeroLong;
+        this.checkIfStringHaveNumber = checkIfStringHaveNumber;
+        this.convertStringToLowerCase = convertStringToLowerCase;
+    }
+
 
     @Override
     public List<TypeVisualContent> getAllTypeVisualContent() {
-        return null;
+        return typeVisualContentRepository.getAllTypeVisualContent();
     }
 
     @Override
     public Optional<TypeVisualContent> getTypeVisualContentById(Long id) {
-        return Optional.empty();
+        checkIfNullOrZeroLong.check(id);
+        Optional<TypeVisualContent> typeVisualContentId = typeVisualContentRepository.getTypeVisualContentById(id);
+        if(!typeVisualContentId.isPresent())
+            System.out.println("NO EXISTE");
+        return typeVisualContentId;
     }
 
     @Override
     public TypeVisualContent saveTypeVisualContent(TypeVisualContent typeVisualContent) {
-        return null;
+        checkIfNullOrEmptyString.check(typeVisualContent.getName());
+        checkIfStringHaveNumber.check(typeVisualContent.getName());
+        String stringConverted = convertStringToLowerCase.convert(typeVisualContent.getName());
+        TypeVisualContent typeVisualContentSaved = typeVisualContentRepository.saveTypeVisualContent(stringConverted);
+        if(typeVisualContentSaved.equals(""))
+            System.out.println("NO SE GUARDOO");
+        return typeVisualContentSaved;
     }
 
     @Override
     public String deleteTypeVisualContentById(Long id) {
-        return null;
+        checkIfNullOrZeroLong.check(id);
+        String responseWithAStringThatShouldNotExist = typeVisualContentRepository.deleteTypeVisualContentById(id);
+        if(!responseWithAStringThatShouldNotExist.isEmpty())
+            System.out.println("NO SE BORRO");
+        return "BORRADO";
     }
 }
